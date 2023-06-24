3f1eb79d58cade7e5763ae26afb0d15936bf0535
bryanltobing <bryantobing0@gmail.com>
2023-06-24 13:16:54 +0800

    feat: cropping actually works

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
-import React, {useCallback, useMemo, useRef} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {
   ActivityIndicator,
+  Dimensions,
+  Image,
+  Platform,
   ScrollView,
   StyleSheet,
   Text,
@@ -17,13 +20,31 @@ import {
   useCameraFormat,
 } from 'react-native-vision-camera';
 import {reduceRatio} from './src/helpers';
+import {manipulateAsync, type ActionCrop} from 'expo-image-manipulator';
+
+export const KTP_WIDTH = 323.52;
+export const KTP_HEIGHT = 204.01;
 
 export default function App() {
   const devices = useCameraDevices();
   const device = devices.back;
   const cameraRef = useRef<Camera | null>(null);
+  const boundingBoxRef = useRef<View | null>(null);
 
-  const {width} = useWindowDimensions();
+  const [cropData, setCropData] = useState<ActionCrop['crop']>();
+
+  const {width, height} = useWindowDimensions();
+
+  // alert(JSON.stringify({width: Dimensions.get('window').width, height: Dimensions.get("window").height}, null, 2));
+
+  const [imageUri, setImageUri] = useState('');
+  // useEffect(() => {
+  //   if (imageUri) {
+  //     Image.getSize(imageUri, (width, height) => {
+  //       alert(JSON.stringify({width, height}, null, 2));
+  //     });
+  //   }
+  // }, [imageUri]);
 
   const availableRatios = useMemo(
     () =>
@@ -45,14 +66,63 @@ export default function App() {
   const cameraFormat = useCameraFormat(device);
 
   const format = useMemo(
-    () => availableRatios?.['16:9']?.[0],
+    () => availableRatios?.['4:3']?.[0],
     [availableRatios],
   );
 
   const handleTakePhoto = useCallback(async () => {
     const photo = await cameraRef.current?.takePhoto({skipMetadata: false});
-    alert(JSON.stringify(photo, null, 2));
-  }, []);
+
+    if (!photo) return;
+    const photoTakenUri = 'file://' + photo.path;
+
+    // hack on ios for flipped width and height
+    const photoCapturedWidth =
+      photo.width > photo.height ? photo.height : photo.width;
+    const photoCapturedHeight =
+      photo.width > photo.height ? photo.width : photo.height;
+
+    setImageUri(photoTakenUri);
+    // alert(
+    //   JSON.stringify(
+    //     {WIDTH: photoCapturedWidth, height: photoCapturedHeight},
+    //     null,
+    //     2,
+    //   ),
+    // );
+
+    if (!cropData) {
+      return;
+    }
+
+    const scaleWidth = photoCapturedWidth / width;
+    const scaleHeight = photoCapturedHeight / (width * (4 / 3));
+
+    const manipulated = await manipulateAsync(photoTakenUri, [
+      {
+        crop: {
+          width: cropData.width * scaleWidth,
+          height: cropData.height * scaleHeight,
+          originX: cropData.originX * scaleWidth,
+          originY: cropData.originY * scaleHeight,
+        },
+      },
+    ]);
+
+    setImageUri(manipulated.uri);
+  }, [cropData, width]);
+
+  if (imageUri) {
+    return (
+      <TouchableOpacity onPress={() => setImageUri('')}>
+        <Image
+          source={{uri: imageUri}}
+          style={{width: '100%', height: '100%'}}
+          resizeMode="contain"
+        />
+      </TouchableOpacity>
+    );
+  }
 
   if (device == null) {
     return <ActivityIndicator />;
@@ -65,31 +135,50 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Camera
-        style={[StyleSheet.absoluteFill, {width, height: width * (16 / 9)}]}
+        style={[StyleSheet.absoluteFill, {width, height: width * (4 / 3)}]}
         device={device}
         isActive={true}
         ref={cameraRef}
         photo
+        zoom={device.neutralZoom}
         format={{
           ...format,
-          // hack on android TODO: need to make sure on ios
-          photoWidth: format.photoHeight,
-          photoHeight: format.photoWidth,
+          // hack on android
+          ...Platform.select({
+            android: {
+              photoWidth: format.photoHeight,
+              photoHeight: format.photoWidth,
+            },
+            // ios: {
+            //   photoWidth: format.photoHeight,
+            //   photoHeight: format.photoWidth,
+            // },
+          }),
         }}
       />
       <View style={[StyleSheet.absoluteFill]}>
         <View style={styles.controlContainer}>
-          <View style={styles.boundingBox} />
-          <ScrollView>
+          <View
+            style={styles.boundingBox}
+            ref={boundingBoxRef}
+            onLayout={() => {
+              boundingBoxRef.current?.measure(
+                (x, y, width, height, pageX, pageY) => {
+                  setCropData({height, width, originX: pageX, originY: pageY});
+                },
+              );
+            }}
+          />
+          {/* <ScrollView>
             <Text style={{color: 'white'}}>
               {JSON.stringify(format, null, 2)}
             </Text>
-          </ScrollView>
+          </ScrollView> */}
 
           <TouchableOpacity
             style={styles.captureButton}
             onPress={handleTakePhoto}>
-            <Text>Capture</Text>
+            <Text style={{color: 'white'}}>Capture</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -103,14 +192,16 @@ const styles = StyleSheet.create({
     position: 'relative',
   },
   controlContainer: {
-    flex: 1,
+    // flex: 1,
     justifyContent: 'space-between',
   },
   boundingBox: {
-    width: 323.52,
-    height: 204.01,
+    // width: KTP_WIDTH,
+    // height: KTP_HEIGHT,
+    aspectRatio: KTP_WIDTH / KTP_HEIGHT,
     borderColor: 'white',
     borderWidth: 2,
+    marginHorizontal: 16,
   },
   captureButton: {
     padding: 16,
